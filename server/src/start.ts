5df1f141846ec6f246c484469d0f6ee99289ed83
Sagar Acharya <acharyasaagar@gmail.com>
2018-12-20 00:50:21 +0200

    Start file added to start the api server, configure it and also connect to the database using api-server class and db-manager class

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,17 +1,39 @@
-import express from 'express';
+import helmet from 'helmet';
+import compression from 'compression';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
-import ApiServer from './ApiServer';
+import apiServer from './api-server';
+import dbManager from './db-manager';
 import config from './config';
-import routes from './routes'
+import routes from './routes';
+import shouldCompress from './middlewares/should-compress'
 
-const app: express.Application = express();
+// Api server instance
+const server = new apiServer(config);
 
-const server = new ApiServer(app, config);
+// Dabatase instance
+const database = new dbManager(config);
+database.establishConnection()
+  .then(connection => {
+    console.log('\n Database Connection established successfully.\n');
 
-// Start Api Server 
+    // Start server and perform basic configurations
+    server.start()
+      .then(app => {
+        // Configure middlewares required for all routes
+        app.use(helmet());
+        app.use(compression({filter: shouldCompress}));
+        app.use(bodyParser.json());
+        app.use(cors());
 
-server.start();
-
-// Hook controllers up with the Api Server
-
-routes(server.app);
+        // Hook controllers up with the Api Server
+        routes(app);
+      })
+      .catch((err: Error) => {
+        console.error(err);
+      });
+  })
+  .catch((err: Error) => {
+    console.error('\n Error connecting to the database.\n');
+  })
