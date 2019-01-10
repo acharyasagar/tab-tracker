3c733be71e0c6db641858095be7557eaa125bf39
Sagar Acharya <acharyasaagar@gmail.com>
2019-01-11 00:06:29 +0200

    FEAT: ( types ) => Adds types for config object

---
+++ server/src/types/config.ts
@@ -0,0 +1,18 @@
+export interface IConfig {
+  port: number;
+  dbConnectionOptions: {
+    type: string;
+    host: string;
+    port: number,
+    database: string;
+    username: string;
+    password: string;
+    synchronize: boolean;
+  };
+  authentication: {
+    jwtSecret: string;
+    jwtOptions?: {
+      expiresIn?: string | number;
+    }
+  };
+}
